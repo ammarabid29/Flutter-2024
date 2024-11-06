3eb5629fdea84b988a271771b596b158b904e1cf
ammarabid29 <aammarawan786@gmail.com>
2024-11-06 18:12:28 +0500

    Basics I (Dice Roller App)

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,33 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors});
+
+  GradientContainer.purple({super.key})
+      : colors = [
+          const Color.fromARGB(255, 25, 1, 67),
+          const Color.fromARGB(255, 51, 22, 100)
+        ];
+
+  final List<Color> colors;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
